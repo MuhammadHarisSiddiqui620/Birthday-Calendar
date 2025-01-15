ecaace038246f7b58c20d684ecf9c92ed8c873ad
Haris <muhammadharissiddiqui33@gmail.com>
2025-01-16 01:15:30 +0500

    birthday_calendor[19]
    Changes
    - PostcardScreen ss implemented
    
    Issues
    - None

--- lib/Screens/PostcardScreen.dart
+++ lib/Screens/PostcardScreen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'package:gal/gal.dart'; // Import the gal package
+
 import 'package:birthday_calendor/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:screenshot/screenshot.dart';
 
 class PostcardScreen extends StatefulWidget {
@@ -15,194 +19,213 @@ class _PostcardScreenState extends State<PostcardScreen> {
   String inputText = "";
   ScreenshotController screenshotController = ScreenshotController();
 
-  // Function to capture the screenshot
   void captureScreenshot() async {
-    // Capture the screenshot of the widget
-    final image = await screenshotController.capture();
+    try {
+      final image = await screenshotController.capture();
+      if (image == null) {
+        print('Screenshot returned null.');
+      } else {
+        final tempDir = await getTemporaryDirectory();
+        final filePath = '${tempDir.path}/screenshot.png';
+        final file = File(filePath);
+        await file.writeAsBytes(image);
 
-    if (image != null) {
-      // You can save the image or share it as needed
-      print('Screenshot captured');
-    } else {
-      print('Screenshot Not captured');
+        // Save to gallery using gal package
+        await Gal.putImage(filePath);
+
+        // Notify user of success
+        print('Screenshot saved to gallery.');
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Screenshot saved to gallery.'),
+        ));
+      }
+    } catch (e) {
+      print('Error capturing screenshot: $e');
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Error capturing screenshot: $e'),
+      ));
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            color: const Color(0xFFBAC8FF),
-          ),
-          ListView(
+      body: Screenshot(
+          child: Stack(
             children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 35, horizontal: 35),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (inputText.isEmpty)
-                      const Text(
-                        "Birthday",
-                        style: TextStyle(
-                          fontSize: 32,
-                          color: Color(0xFF141522),
-                          fontFamily: 'PlusJakartaSans',
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    if (inputText.isEmpty) const SizedBox(height: 8),
-                    if (inputText.isEmpty)
-                      const Text(
-                        "Choose your congratulations!",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Color(0xFF54577A),
-                          fontFamily: 'PlusJakartaSans',
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    const SizedBox(height: 17),
-                    if (selectedImage == null)
-                      Wrap(
-                        spacing: 10, // Horizontal spacing between cards
-                        runSpacing: 40, // Vertical spacing between rows
-                        children: List.generate(6, (index) {
-                          String imagePath =
-                              'images/post_card_${(index % 6) + 1}.png';
-                          return GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                selectedImage =
-                                    imagePath; // Update selected image
-                              });
-                            },
-                            child: Image.asset(
-                              imagePath,
-                              width: 150,
-                              height: 150,
-                              fit: BoxFit.cover,
-                            ),
-                          );
-                        }),
-                      )
-                    else
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.white, width: 3),
-                            ),
-                            child: Image.asset(
-                              selectedImage!,
-                              width: 347, // Larger width
-                              height: 214, // Larger height
-                              fit: BoxFit.cover,
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: const Color(0xFFBAC8FF),
+              ),
+              ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 35, horizontal: 35),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (inputText.isEmpty)
+                          const Text(
+                            "Birthday",
+                            style: TextStyle(
+                              fontSize: 32,
+                              color: Color(0xFF141522),
+                              fontFamily: 'PlusJakartaSans',
+                              fontWeight: FontWeight.w600,
                             ),
                           ),
-                          const SizedBox(height: 26),
-                          if (inputText.isEmpty) ...[
-                            TextField(
-                              minLines: 5,
-                              maxLines: null,
-                              controller: nameController,
-                              style: TextStyle(fontSize: 16),
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-                                contentPadding:
-                                    EdgeInsets.fromLTRB(20, 20, 20, 20),
-                                border: OutlineInputBorder(
-                                  borderSide: BorderSide.none,
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(16),
-                                  ),
-                                ),
-                                hintText: '...or write your own text',
-                                hintStyle: postCardInputText,
-                              ),
+                        if (inputText.isEmpty) const SizedBox(height: 8),
+                        if (inputText.isEmpty)
+                          const Text(
+                            "Choose your congratulations!",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xFF54577A),
+                              fontFamily: 'PlusJakartaSans',
+                              fontWeight: FontWeight.w500,
                             ),
-                            const SizedBox(height: 16),
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                minimumSize: Size(100, 52),
-                                backgroundColor: Color(0xFF152C8D),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30),
+                          ),
+                        const SizedBox(height: 17),
+                        if (selectedImage == null)
+                          Wrap(
+                            spacing: 10, // Horizontal spacing between cards
+                            runSpacing: 40, // Vertical spacing between rows
+                            children: List.generate(6, (index) {
+                              String imagePath =
+                                  'images/post_card_${(index % 6) + 1}.png';
+                              return GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    selectedImage =
+                                        imagePath; // Update selected image
+                                  });
+                                },
+                                child: Image.asset(
+                                  imagePath,
+                                  width: 150,
+                                  height: 150,
+                                  fit: BoxFit.cover,
+                                ),
+                              );
+                            }),
+                          )
+                        else
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: Colors.white, width: 3),
+                                ),
+                                child: Image.asset(
+                                  selectedImage!,
+                                  width: 347, // Larger width
+                                  height: 214, // Larger height
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                              onPressed: () {
-                                setState(() {
-                                  if (nameController.text.isNotEmpty) {
-                                    inputText = nameController.text;
-                                  }
-                                });
-                              },
-                              child: const Text(
-                                "Save",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ),
-                          ] else
-                            Column(
-                              children: [
-                                Container(
-                                  decoration: BoxDecoration(
-                                    border: Border.all(
-                                        color: Colors.white, width: 2),
-                                    borderRadius: BorderRadius.circular(16),
-                                    color: Color(0xFF152C8D),
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(20.0),
-                                    child: Text(
-                                      inputText,
-                                      style: TextStyle(
-                                        color: Color(0xFFF3F3F3),
-                                        fontSize: 16,
-                                        fontFamily: 'PlusJakartaSans',
-                                        fontWeight: FontWeight.w300,
+                              const SizedBox(height: 26),
+                              if (inputText.isEmpty) ...[
+                                TextField(
+                                  minLines: 5,
+                                  maxLines: null,
+                                  controller: nameController,
+                                  style: TextStyle(fontSize: 16),
+                                  decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: Colors.white,
+                                    contentPadding:
+                                        EdgeInsets.fromLTRB(20, 20, 20, 20),
+                                    border: OutlineInputBorder(
+                                      borderSide: BorderSide.none,
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(16),
                                       ),
                                     ),
+                                    hintText: '...or write your own text',
+                                    hintStyle: postCardInputText,
                                   ),
                                 ),
-                                SizedBox(
-                                  height: 32,
-                                ),
-                                GestureDetector(
-                                  onTap:
-                                      captureScreenshot, // Capture screenshot on tap
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(48.0),
+                                const SizedBox(height: 16),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(100, 52),
+                                    backgroundColor: Color(0xFF152C8D),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30),
                                     ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Icon(
-                                        Icons.file_download,
-                                        size: 36,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      if (nameController.text.isNotEmpty) {
+                                        inputText = nameController.text;
+                                      }
+                                    });
+                                  },
+                                  child: const Text(
+                                    "Save",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ] else
+                                Column(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.white, width: 2),
+                                        borderRadius: BorderRadius.circular(16),
+                                        color: Color(0xFF152C8D),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(20.0),
+                                        child: Text(
+                                          inputText,
+                                          style: TextStyle(
+                                            color: Color(0xFFF3F3F3),
+                                            fontSize: 16,
+                                            fontFamily: 'PlusJakartaSans',
+                                            fontWeight: FontWeight.w300,
+                                          ),
+                                        ),
                                       ),
                                     ),
-                                  ),
+                                    SizedBox(
+                                      height: 32,
+                                    ),
+                                    GestureDetector(
+                                      onTap:
+                                          captureScreenshot, // Capture screenshot on tap
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(48.0),
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Icon(
+                                            Icons.file_download,
+                                            size: 36,
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ],
                                 )
-                              ],
-                            )
-                        ],
-                      ),
-                  ],
-                ),
+                            ],
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
+          controller: screenshotController),
     );
   }
 }
